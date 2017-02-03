d2f3680c9d955da3c695eab309cedc4ef904162b
yadi13 <thesuy13@gmail.com>
2017-02-03 18:05:45 +0700

    menambah rumus lingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -3,6 +3,10 @@ class HitungLuas2 {
 	public static int cariLuasPersegiPanjang(int panjang, int lebar) {
 		return panjang * lebar;
 	}
+public static float cariLuasLingkaran(int jarijari) {
+		return (22 * jarijari * jarijari) / 7;
+	}
+
 
 	public static void main (String[] args) {
 
